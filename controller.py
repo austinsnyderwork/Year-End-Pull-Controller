cfe25504ea0a52f8ffc9f6453855e8f24fd03aea
austinsnyderwork <179244141+austinsnyderwork@users.noreply.github.com>
2025-11-24 16:18:09 -0600

    c

--- controller.py
+++ controller.py
@@ -2,7 +2,7 @@ import logging
 from pathlib import Path
 import pandas as pd
 
-from sqlalchemy import create_engine
+import hpinv_sql_pull
 import hpinv_history_analysis
 
 
@@ -18,12 +18,7 @@ class YearEndPullController:
         self.start_year = pull_start_year
         self.end_year = pull_end_year
 
-        server = 'HC-SQL5'
-        database = 'HPINV'
-        driver = 'ODBC+Driver+17+for+SQL+Server'
-        self.engine = create_engine(
-            f"mssql+pyodbc://{server}/{database}?driver={driver}&trusted_connection=yes"
-        )
+        self.sql_manager = hpinv_sql_pull.SqlManager()
 
     def pull_year_end(
         self,
@@ -31,18 +26,17 @@ class YearEndPullController:
     ) -> pd.DataFrame:
         logging.info("Beginning year end SQL pull.")
 
+        transactions_df = self.sql_manager.pull(
+            query_spec=hpinv_sql_pull.TransactionsSpec(start_year=self.start_year)
+        )
         if check_for_errors:
             logging.info("Checking for errors before pulling year end.")
             errors_manager = hpinv_history_analysis.ProgramManager(
-                transactions_df=
+                transactions_df=transactions_df
             )
 
-        with open("queries/define_work_periods.sql", "r") as f:
-            work_periods_query = f.read()
-        work_periods_df = pd.read_sql(
-            work_periods_query,
-            con=self.engine,
-            params=[self.start_year]
+        work_periods_df = self.sql_manager.pull(
+            query_spec=hpinv_sql_pull.WorkPeriodsSpec()
         )
 
         pull_years = list(range(self.start_year, self.end_year + 1))
@@ -52,11 +46,8 @@ class YearEndPullController:
                 (month_equivalent >= work_periods_df['MonthsSinceAdd'])
                 & (month_equivalent <= work_periods_df['MonthsSinceDel'])]
 
-        with open("queries/worksite_histories_data.sql", "r") as f:
-            worksite_histories_query = f.read()
-        worksite_histories_data_df = pd.read_sql(
-            worksite_histories_query,
-            con=self.engine
+        worksite_histories_df = self.sql_manager.pull(
+            query_spec=hpinv_sql_pull.WorksiteHistorySpec()
         )
 
         logging.info("Finished year end SQL pull.")
